d790cc7d55ef6f864fe40f5fc4a59354b1148788
Cüneyt Güley <cuneyt.guley@outlook.com>
2021-03-16 02:48:55 +0300

    16.03.2021_2

---
+++ src/app/models/rental.ts
@@ -0,0 +1,12 @@
+export interface Rental{
+    carId:number;
+    modelYear:number;
+    colorName:string;
+    brandName:string;
+    description:string;
+    companyName:string;
+    rentDate:any;
+    returnDate:any;
+    firstName:string;
+    lastName:string;
+}
